546bf0ed10140d90f5f92fd252e79362187dd4a1
Dāvis Čeirāns <cheiraans@gmail.com>
2020-07-15 20:13:43 +0300

    Included more responsive behaviour, fixed navigation button

---
+++ Sem.lv_test/assets/js/theme.js
@@ -0,0 +1,13 @@
+$('#navbarToggle').click(function(){
+  if( $("#navbar").css('display') == 'none') {
+    $('#navbar').attr("style", "display: block !important; flex-basis: 100%; flex-grow: 1; align-items: center;");
+    $('.navbar').attr("style","flex-wrap: wrap; display: flex;");
+    $('#navbar .lang-mobile').attr("style", "display: block;");
+    $('.navbar-nav').attr("style", "flex-direction: column;");
+  } else {
+    $('#navbar').attr("style", "");
+    $('.navbar').attr("style","");
+    $('#navbar .lang-mobile').attr("style", "");
+    $('.navbar-nav').attr("style", "");
+  }
+});
