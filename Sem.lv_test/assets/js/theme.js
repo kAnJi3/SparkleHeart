654f5dc035d18ee354cd888ac8d4357a623c1acf
Dāvis Čeirāns <cheiraans@gmail.com>
2020-07-16 13:26:01 +0300

    Added scroll down button functionality, Edited Dreams section responsive behaviour

--- Sem.lv_test/assets/js/theme.js
+++ Sem.lv_test/assets/js/theme.js
@@ -1,14 +1,18 @@
 $('#navbarToggle').click(function(){
   if( $("#navbar").css('display') == 'none') {
     $('#navbar').attr("style", "display: block !important; flex-basis: 100%; flex-grow: 1; align-items: center;");
-    $('.navbar').attr("style","flex-wrap: wrap; display: flex;");
+    $('.navbar').attr("style","flex-wrap: wrap; display: flex; height: 100vh;");
     $('#navbar .lang-mobile').attr("style", "display: block;");
     $('.navbar-nav').attr("style", "flex-direction: column;");
+    $('#navbarToggle span').removeClass("d-none");
+    $('#navbarToggle span:first').addClass("d-none");
   } else {
     $('#navbar').attr("style", "");
     $('.navbar').attr("style","");
     $('#navbar .lang-mobile').attr("style", "");
     $('.navbar-nav').attr("style", "");
+    $('#navbarToggle span').addClass("d-none");
+    $('#navbarToggle span:first').removeClass("d-none");
   }
 });
 $( window ).resize(function() {
@@ -17,5 +21,22 @@ $( window ).resize(function() {
     $('.navbar').attr("style","");
     $('#navbar .lang-mobile').attr("style", "");
     $('.navbar-nav').attr("style", "");
+    $('#navbarToggle span').addClass("d-none");
+    $('#navbarToggle span:first').removeClass("d-none");
   }
+});
+$(window).scroll(function(){
+  if($(window).scrollTop() === 0 || ( $('.Supported-by').offset().top + $('.Supported-by').height() <  $(window).scrollTop() + $(window).height() )) {
+     $('.Scrolldown').addClass("d-none");
+   }else {
+     $('.Scrolldown').removeClass("d-none");
+   }
+});
+
+$('#ScrollButton').click(function(){
+  event.preventDefault();
+  $('html, body').animate({
+          scrollTop: $('.Contact-us').offset().top + $('.Supported-by').innerHeight() + $('.Contact-us').innerHeight()  - $('.Contact-us').height()
+        },1000, function() {
+          });
 });
