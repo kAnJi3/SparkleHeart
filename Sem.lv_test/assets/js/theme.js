b2f0e90c2bc0b5b205e9ce0de588df8359bbcab6
Dāvis Čeirāns <cheiraans@gmail.com>
2020-07-16 16:28:04 +0300

    More responsive behaviour

--- Sem.lv_test/assets/js/theme.js
+++ Sem.lv_test/assets/js/theme.js
@@ -1,11 +1,12 @@
 $('#navbarToggle').click(function(){
   if( $("#navbar").css('display') == 'none') {
-    $('#navbar').attr("style", "display: block !important; flex-basis: 100%; flex-grow: 1; align-items: center;");
+    $('#navbar').attr("style", "display: block !important; flex-basis: 100%; flex-grow: 1; align-items: center; align-self: flex-start;");
     $('.navbar').attr("style","flex-wrap: wrap; display: flex; height: 100vh;");
     $('#navbar .lang-mobile').attr("style", "display: block;");
     $('.navbar-nav').attr("style", "flex-direction: column;");
     $('#navbarToggle span').removeClass("d-none");
     $('#navbarToggle span:first').addClass("d-none");
+    $('.Scrolldown').addClass("d-none");
   } else {
     $('#navbar').attr("style", "");
     $('.navbar').attr("style","");
